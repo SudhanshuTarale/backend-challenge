74283db6de0ecdf782a2ba95d9728d1d7a06b660
SudhanshuTarale <47272414+SudhanshuTarale@users.noreply.github.com>
2024-03-29 14:50:00 -0400

    added router urls

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import TaskViewSet, LabelViewSet
+
+router = routers.DefaultRouter()
+router.register(r'tasks', TaskViewSet)
+router.register(r'labels', LabelViewSet)
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
